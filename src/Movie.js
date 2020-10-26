4a5b9a80bc9f1fd7e53b30148c9e8f2ae04fdcd2
Brian <briandsalemi@gmail.com>
2020-10-25 22:18:29 -0400

    Created the likesCount and dislikesCount state in Movie component. Added onClick event listeners to like and dislike buttons to increment the likesCount and dislikesCount when clicked

--- src/Movie.js
+++ src/Movie.js
@@ -1,14 +1,19 @@
 import React from "react";
+import {useState} from "react"
 
 const Movie = (props) => {
     const {title, image, likes, dislikes} = props
+    const [likesCount, setLikesCount] = useState(likes)
+    const [dislikesCount, setDislikesCount] = useState(dislikes)
+
     return(
         <div className="movieCard">
             <h3>{title}</h3>
             <img src={image} alt="movie-poster" className="movieImage"/>
             <div className="reviewButtonsContainer">
-               <button> Like </button>{likes}
-               <button> Dislike</button>{dislikes}
+                
+               <button onClick={() => setLikesCount(likesCount + 1)}> Like </button>{likesCount}
+               <button onClick={() => setDislikesCount(dislikesCount + 1)}> Dislike</button>{dislikesCount}
             </div>
         </div>
     )
